f909543202fc146833acc254431b7de14afeee12
turtle-insect <turtle.insect@gmail.com>
2024-11-06 03:54:10 +0900

    refactor

--- GvasViewer/Util/Zlib.cs
+++ GvasViewer/Util/Zlib.cs
@@ -6,36 +6,22 @@ namespace GvasViewer.Util
 	{
 		public static Byte[] Compression(Byte[] buffer)
 		{
-			Byte[] result = Array.Empty<byte>();
-			using (var input = new MemoryStream(buffer))
-			{
-				using (var output = new MemoryStream())
-				{
-					using (var zlib = new System.IO.Compression.ZLibStream(output, System.IO.Compression.CompressionLevel.Fastest))
-					{
-						input.CopyTo(zlib);
-					}
-					result = output.ToArray();
-				}
-			}
-			return result;
+			using var input = new MemoryStream(buffer);
+			using var output = new MemoryStream();
+			using var zlib = new System.IO.Compression.ZLibStream(output, System.IO.Compression.CompressionLevel.Fastest);
+			input.CopyTo(zlib);
+
+			return output.ToArray();
 		}
 
 		public static Byte[] Decompress(Byte[] buffer)
 		{
-			Byte[] result = Array.Empty<byte>();
-			using (var input = new MemoryStream(buffer))
-			{
-				using (var zlib = new System.IO.Compression.ZLibStream(input, System.IO.Compression.CompressionMode.Decompress))
-				{
-					using (var output = new MemoryStream())
-					{
-						zlib.CopyTo(output);
-						result = output.ToArray();
-					}
-				}
-			}
-			return result;
+			using var input = new MemoryStream(buffer);
+			using var zlib = new System.IO.Compression.ZLibStream(input, System.IO.Compression.CompressionMode.Decompress);
+			using var output = new MemoryStream();
+			zlib.CopyTo(output);
+
+			return output.ToArray();
 		}
 	}
 }
