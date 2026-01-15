d5330d6ebc970c2a96132cb087d7e2ca44ac54da
turtle-insect <turtle.insect@gmail.com>
2026-01-15 19:01:58 +0900

    refactor

--- GvasViewer/FileFormat/Platform/Platform.cs
+++ GvasViewer/FileFormat/Platform/Platform.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GvasViewer.FileFormat.Platform
+﻿namespace GvasViewer.FileFormat.Platform
 {
 	internal enum Platform
 	{
