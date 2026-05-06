a28ded19d4c737f193cf5e8f70229e2399cb0480
turtle-insect <turtle.insect@gmail.com>
2026-05-07 03:23:26 +0900

    support multi rootProperty

---
+++ Gvas/Property/GvasRootProperty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gvas.Property
+{
+	internal class GvasRootProperty : GvasProperty
+	{
+		public GvasRootProperty()
+			: base()
+		{ }
+
+		public GvasRootProperty(GvasRootProperty property)
+			: base(property)
+		{ }
+
+		public override GvasProperty Clone()
+		{
+			return new GvasRootProperty(this);
+		}
+
+		public override object Value
+		{
+			get => throw new NotImplementedException();
+			set => throw new NotImplementedException();
+		}
+
+		public override void Read(BinaryReader reader)
+		{
+			for (; ; )
+			{
+				var property = Util.ReadProperty(reader);
+				property.Read(reader);
+				AppendChildren(property);
+				if (property is GvasNoneProperty) break;
+			}
+			reader.ReadBytes(4);
+		}
+
+		public override void Write(BinaryWriter writer)
+		{
+			foreach (var property in Children)
+			{
+				property.Write(writer);
+			}
+			writer.Write(0);
+		}
+
+		public override void ReadValue(BinaryReader reader)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void WriteValue(BinaryWriter writer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
