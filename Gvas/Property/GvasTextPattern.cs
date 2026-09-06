d2c9a80f517beb1f3867f0548bde766d5bb41014
turtle-insect <turtle.insect@gmail.com>
2026-09-07 02:50:14 +0900

    refactor

--- Gvas/Property/GvasTextPattern.cs
+++ Gvas/Property/GvasTextPattern.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml.Linq;
-
-namespace Gvas.Property
+﻿namespace Gvas.Property
 {
 	internal abstract class GvasTextPattern
 	{
