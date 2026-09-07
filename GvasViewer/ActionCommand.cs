99608ad5a3d2a5c27513aa4fee7902588ea0df8a
turtle-insect <turtle.insect@gmail.com>
2026-09-08 01:36:32 +0900

    use primary constructor

--- GvasViewer/ActionCommand.cs
+++ GvasViewer/ActionCommand.cs
@@ -2,15 +2,13 @@
 
 namespace GvasViewer
 {
-	class ActionCommand : ICommand
+	class ActionCommand(Action<Object?> _action) : ICommand
 	{
 #pragma warning disable CS0067
 		public event EventHandler? CanExecuteChanged;
 #pragma warning restore CS0067
 
-		private readonly Action<Object?> mAction;
-		public ActionCommand(Action<Object?> action) => mAction = action;
 		public bool CanExecute(Object? parameter) => true;
-		public void Execute(Object? parameter) => mAction.Invoke(parameter);
+		public void Execute(Object? parameter) => _action(parameter);
 	}
 }
